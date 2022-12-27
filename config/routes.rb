cc98284b5d593a7f079f44f2c6f33a000db43e3d
Daniel Ansari <d0nsari2020@gmail.com>
2022-12-27 16:05:29 +0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   post 'create-verification-session', to: 'verification_sessions#create'
   get 'verification-webhook', to: 'verification_sessions#webhook'
+  post 'verification-webhook', to: 'verification_sessions#webhook'
+  put 'verification-webhook', to: 'verification_sessions#webhook'
   post 'verifications', to: 'verification_sessions#search'
   get 'verifications', to: 'verification_sessions#index'
   get 'verifications/:id', to: 'verification_sessions#show', as: :retrieve_verification
